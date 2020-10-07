eb36a99bdb3d220687c0689c41e5081ee47058ac
Gil Grigorian <Gigasgamesofficial@gmail.com>
2020-10-07 23:31:55 +0300

    Added Mine + Explosion

---
+++ GunsAndSpells/Assets/Scripts/MineCollision.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineCollision : MonoBehaviour
+{
+    public GameObject explosion;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            explosion.SetActive(true);
+            WaitSeconds();
+            explosion.SetActive(false);
+
+
+        }
+    }
+
+    IEnumerator WaitSeconds()
+    {
+        yield return new WaitForSeconds(2);
+    }
+}
