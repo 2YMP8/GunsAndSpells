0d9f180b87bfba1eec3a9a15924ac43af480c7be
Ytshak Perez <itshak.perez@gmail.com>
2020-10-12 18:07:44 +0300

    Avatar UpDate (Player)
    2 Power ups(fire,ice)

--- GunsAndSpells/Assets/YmpScripts/AvatarEngine.cs
+++ GunsAndSpells/Assets/YmpScripts/AvatarEngine.cs
@@ -25,6 +25,21 @@ public class AvatarEngine : MonoBehaviour
 
     private int _onClicks;
     private float _lastTimeClicked, _maxComboDelay;
+    private bool _fightAttak;
+
+    public Transform powerUpPoint;
+
+    private bool _fierBallAttack;
+    public GameObject fierBallPrefab;
+    private GameObject _fierBall;
+
+    private bool _iceLanceAttack;
+    public GameObject iceLancePrefab;
+    private GameObject _iceLance;
+
+    private string[] _powerUps = new string[3];
+    public static bool powerUpsFull;
+
     #endregion
 
     void Start()
@@ -41,6 +56,12 @@ public class AvatarEngine : MonoBehaviour
         _maxComboDelay = 1.5f;
         _maxJampDelay = 0.8f;
 
+        _fierBallAttack = false;
+        _iceLanceAttack = false;
+        _fightAttak = true;
+
+        _powerUps[0] = "FightMode";
+        powerUpsFull = false;
     }
 
 
@@ -97,8 +118,18 @@ public class AvatarEngine : MonoBehaviour
         if (Input.GetMouseButtonDown(0))
         {
             _lastTimeClicked = Time.time;
-            _onClicks++;
-            _onClicks = Mathf.Clamp(_onClicks,0, 3);
+            if (_fightAttak)
+            {
+                _maxComboDelay = 1.5f;
+                _onClicks++;
+                _onClicks = Mathf.Clamp(_onClicks, 0, 3);
+            }
+            else
+            {
+                _onClicks = 1;
+                _maxComboDelay = 0.1f;
+            }
+
 
            /* if (_onClicks == 1)
             {
@@ -116,12 +147,24 @@ public class AvatarEngine : MonoBehaviour
 
             }*/
         }
-        if(Time.time - _lastTimeClicked > _maxComboDelay)
+        if (Time.time - _lastTimeClicked > _maxComboDelay)
         {
             _onClicks = 0;
         }
 
-    
+        if (Input.GetKeyDown(KeyCode.Alpha1))//Fight combo
+        {
+            ModeSwitch(0);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))//Scond powerup
+        {
+            ModeSwitch(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))//Therd poerup 
+        {
+            ModeSwitch(2);
+        }
+
     }
 
     private void MovePlayer(Vector3 sidesRefrence, Vector3 forwardRefrence)
@@ -144,7 +187,7 @@ public class AvatarEngine : MonoBehaviour
         _anim.SetInteger("Jump", _jump);
         _anim.SetBool("IsGrounded", _isGrounded);
         _anim.SetInteger("Attack", _onClicks);
-        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("KB_p_OneTwo"))
+        if (_anim.GetCurrentAnimatorStateInfo(0).IsName("KB_p_OneTwo") || !_fightAttak)
         {
             _anim.SetBool("OneTwo", false);
         }
@@ -152,6 +195,8 @@ public class AvatarEngine : MonoBehaviour
         {
             _anim.SetBool("OneTwo", true);
         }
+        _anim.SetBool("FierBallAttack", _fierBallAttack);
+        _anim.SetBool("IceLanceAttack", _iceLanceAttack); 
     }
 
     private void CheckIsGrounded()
@@ -164,10 +209,79 @@ public class AvatarEngine : MonoBehaviour
         }
     }
 
+    //function calld by animation
     private void MyAddForce()
     {
-        rb.AddForce(transform.up * 300);
+        rb.AddForce(transform.up * 500);
         //rb.AddForce(transform.forward * 500);
+    }
+
+    private void FierBallFunc()
+    {
+        _fierBall = Instantiate(fierBallPrefab, powerUpPoint.position, Quaternion.identity);
+        _fierBall.GetComponent<Rigidbody>().AddForce(transform.forward * 300,ForceMode.Acceleration);
+        Destroy(_fierBall.gameObject,2);
+    }
+
+    private void IceLanceFunc()
+    {
+        _iceLance = Instantiate(iceLancePrefab, powerUpPoint.position, transform.rotation);
+       // _iceLance.GetComponent<Rigidbody>().AddForce(transform.forward * 500, ForceMode.Acceleration);
+        Destroy(_iceLance.gameObject, 2);
+    }
+    //til here.
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("FireMode"))
+        {
+            CheckSpaceInPowerUps("FireMode");
+        }
+        else if (other.CompareTag("IceMode"))
+        {
+            CheckSpaceInPowerUps("IceMode");
+        }
+    }
+
+    private void CheckSpaceInPowerUps(string power)
+    {
+        if (_powerUps[1] == null)
+        {
+            _powerUps[1] = power;
+        }
+        else if(_powerUps[2] == null)
+        {
+            _powerUps[2] = power;
+            powerUpsFull = true;
+        }
+    }
+
+    private void ModeSwitch(int i)
+    {
+        switch (_powerUps[i])
+        {
+            case "FightMode":
+                _fightAttak = true;
+                _fierBallAttack = false;
+                _iceLanceAttack = false;
+                break;
+
+            case "FireMode":
+                _fightAttak = false;
+                _fierBallAttack = true;
+                _iceLanceAttack = false;
+                break;
+
+            case "IceMode":
+                _fightAttak = false;
+                _fierBallAttack = false;
+                _iceLanceAttack = true;
+                break;
+
+            default:
+                break;
+        }
     }
 
     //private void OnCollisionEnter(Collision collision)
@@ -180,23 +294,23 @@ public class AvatarEngine : MonoBehaviour
     //}
 
     #region TimerHit
-/*    IEnumerator TimerHit1()
-    {
-        yield return new WaitForSeconds(0.8f);
-        SoundManager.PlaySound("Hit1");
+    /*    IEnumerator TimerHit1()
+        {
+            yield return new WaitForSeconds(0.8f);
+            SoundManager.PlaySound("Hit1");
 
-    }
-    IEnumerator TimerHit2()
-    {
-        yield return new WaitForSeconds(0.2f);
-        SoundManager.PlaySound("Hit2");
+        }
+        IEnumerator TimerHit2()
+        {
+            yield return new WaitForSeconds(0.2f);
+            SoundManager.PlaySound("Hit2");
 
-    }
-    IEnumerator TimerHit3()
-    {
-        yield return new WaitForSeconds(0.2f);
-        SoundManager.PlaySound("Hit3");
+        }
+        IEnumerator TimerHit3()
+        {
+            yield return new WaitForSeconds(0.2f);
+            SoundManager.PlaySound("Hit3");
 
-    }*/
+        }*/
     #endregion
 }
---
+++ GunsAndSpells/Assets/YmpScripts/PowerUp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    public GameObject smokeEffectPrefab;
+    public Transform inPosition;
+    private GameObject _smokeEffect;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            PickUp();
+        }
+    }
+
+    private void PickUp()
+    {
+        if (!AvatarEngine.powerUpsFull)
+        {
+            _smokeEffect = Instantiate(smokeEffectPrefab, inPosition.position, transform.rotation);
+            Destroy(gameObject);
+            Destroy(_smokeEffect.gameObject, 1.5f);
+        }
+    }
+}
